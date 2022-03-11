53c621fb4f6d78775613a8eb43ace100abe50cec
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2022-03-11 00:29:06 +0000

    feat: create paid program

--- main.cpp
+++ main.cpp
@@ -3,5 +3,16 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!\n";
+  float paidValue, productValue, restValue = 0;
+
+  cout << "Informe o valor pago: \n";
+  cin >> paidValue;
+
+  cout << "Informe o valor do produto: \n";
+  cin >> productValue;
+
+  restValue = paidValue - productValue;
+
+  cout << "Seu troco é: " << restValue;
+  return 0;
 }
