88d65a2c80c734b1b159e2ef464aa7e1ea7e2773
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-03-17 20:09:09 -0300

    feat: salary

---
+++ 17-03-2022/exercise-four/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  float salary, invoceOne, invoceTwo, restSalary = 0;
+
+  cout << "Informe o salário:\n";
+  cin >> salary;
+
+  cout << "Informe o valor primeira conta:\n";
+  cin >> invoceOne;
+
+  cout << "Informe o valor segunda conta:\n";
+  cin >> invoceTwo;
+
+  restSalary = salary - ((invoceOne * 1.02) + (invoceTwo * 1.02));
+
+  cout << "Restou " << restSalary << " do seu salário";
+
+  return 0;
+}
