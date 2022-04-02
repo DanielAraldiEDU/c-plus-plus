af98ed8188ccba1bd175891c7da3fb6bc22a5017
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-04-02 11:54:13 -0300

    fix: add validation to date

--- 17-03-2022/exercise-eight/main.cpp
+++ 17-03-2022/exercise-eight/main.cpp
@@ -24,11 +24,39 @@ int main()
   cout << "Informe o dia atual:\n";
   cin >> currentDay;
 
+  if (birthYear < 0 || currentYear < 0)
+  {
+    cout << "O ano da data informada é inválido";
+
+    return 0;
+  }
+
+  if ((birthMonth < 1 || birthMonth > 12) || (currentMonth < 1 || currentMonth > 12))
+  {
+    cout << "O mês da data informada é inválido";
+
+    return 0;
+  }
+
+  if (birthMonth % 2 == 1 && (birthDay < 1 || birthDay > 31) ||
+      currentMonth % 2 == 1 && (currentDay < 1 || currentDay > 31) ||
+      birthMonth % 2 == 0 && (birthDay < 1 || birthDay > 30) ||
+      currentMonth % 2 == 0 && (currentDay < 1 || currentDay > 30) ||
+      birthMonth == 2 && birthYear % 4 == 0 && (birthDay < 1 || birthDay > 29) ||
+      currentMonth == 2 && currentYear % 4 != 0 && (currentDay < 1 || currentDay > 28))
+  {
+    cout << "O dia da data informada é inválido";
+
+    return 0;
+  }
+
   age = currentYear - birthYear;
 
   if (age >= 0)
   {
-    if (currentMonth > birthMonth || (currentMonth == birthMonth && currentDay >= birthDay))
+    if (currentMonth > birthMonth ||
+        (currentMonth == birthMonth && currentDay >= birthDay) ||
+        (age == 0 && currentMonth == birthMonth && currentDay < birthDay))
     {
       cout << "Idade: " << age;
     }
