b4464f6068cf210bce7209ad07e225b03b23345e
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-04-30 13:34:38 -0300

    feat: start learning arrays

---
+++ 28-04-2022/exercise-one/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+#define AMOUNT 10
+
+int main()
+{
+  int numbers[AMOUNT] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int smallestNumber, higherNumber;
+  float media = 0, sum = 0;
+
+  for (int i = 0; i < AMOUNT; i++)
+  {
+    if (i == 0 || numbers[i] < smallestNumber)
+    {
+      smallestNumber = numbers[i];
+    }
+
+    if (i == 0 || numbers[i] > higherNumber)
+    {
+      higherNumber = numbers[i];
+    }
+
+    sum += numbers[i];
+  }
+
+  media = sum / AMOUNT;
+
+  cout << "\nO menor número é: " << smallestNumber;
+  cout << "\nO maior número é: " << higherNumber;
+  cout << "\nA média dos valores é " << media;
+
+  return 0;
+}
