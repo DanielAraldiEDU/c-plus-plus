e7990a2bce0013101bb3dede276c76d94272eeb7
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-08-01 19:44:10 -0300

    feat: enter a number and show on terminal

---
+++ 01-08-2022/exercise-one/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int number;
+
+  cout << "Enter a number:\n";
+  cin >> number;
+
+  cout << "The number you entered is: " << number;
+
+  return 0;
+}
