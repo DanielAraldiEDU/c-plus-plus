aed9baff060022b9d786ca765535e322eb74ec7b
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-08-01 19:51:52 -0300

    feat: making the sum of the two numbers

---
+++ 01-08-2022/exercise-two/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int numberOne, numberTwo;
+
+  cout << "Enter a first number:\n";
+  cin >> numberOne;
+
+  cout << "\nEnter a second number:\n";
+  cin >> numberTwo;
+
+  cout << "\nThe sum of the two numbers is: " << numberOne + numberTwo;
+
+  return 0;
+}
