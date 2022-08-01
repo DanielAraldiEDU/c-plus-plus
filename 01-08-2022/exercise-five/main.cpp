1cb363f344628041b3de85e0cbd1a4c1bc643565
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-08-01 20:31:44 -0300

    feat: calculete other area

---
+++ 01-08-2022/exercise-five/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+#define height 4
+#define width 6
+
+int main()
+{
+  float area = (width * height) * 2;
+
+  cout << "Área quadrada dobrada: " << area;
+
+  return 0;
+}
