7f1764dc69b9601e0ceaf205101a49106858b47a
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-09-01 19:30:10 -0300

    feat: studying creating and wrinting if exists a file

---
+++ 01-09-2022/exercise-one/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+  ofstream file;
+
+  file.open("file.md");
+  file << "### File.md\n\nCreating or wrinting if exists on file.md\n";
+  file.close();
+
+  return 0;
+}
