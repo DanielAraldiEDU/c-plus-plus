7f4b0e323e138cc6b351e285bc63eacca3f55dd9
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-09-01 19:58:12 -0300

    feat: studying opening file created

---
+++ 01-09-2022/exercise-two/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+  ofstream file;
+
+  file.open("file.bin", ios::out | ios::app | ios::binary);
+
+  if (file.is_open())
+    cout << "File is opening!";
+
+  return 0;
+}
