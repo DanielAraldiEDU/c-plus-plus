8b847e6d4993a696a5d516fc3d308f7a251e0a4f
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-10-01 15:29:11 -0300

    feat: sort numbers

---
+++ 29-09-2022/exercise-one/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int compare(const void *value, const void *valueToCompare)
+{
+  return (*(int *)value - *(int *)valueToCompare);
+}
+
+int main()
+{
+  int array[] = {40, 10, 100, 90, 20, 25};
+
+  qsort(array, 6, sizeof(int), compare);
+
+  for (int i = 0; i < 6; i++)
+    cout << array[i] << " ";
+
+  return 0;
+}
