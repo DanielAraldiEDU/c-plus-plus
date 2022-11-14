6c9b92b814b41f94c6cfc92806ee4c034d6047ff
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-11-14 20:37:49 -0300

    feat: understanding selection sort

---
+++ 14-11-2022/exercise-three/main.h
@@ -0,0 +1,13 @@
+#define LENGTH 10
+
+template <typename T>
+T createRandomNumber(T number);
+template <typename T>
+T *handleSelectionSort(T *array, int length);
+template <typename T>
+T *handlePopulateArray(T *array, int length);
+template <typename T>
+void handleSwapArrayElement(T *elementForExchange, T *element);
+
+template <typename T>
+void printArray(T *array, int length);
