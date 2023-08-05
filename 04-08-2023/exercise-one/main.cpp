34094671bfbf8ad2c3feffa9668ec62d64380e8f
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-08-05 20:34:16 -0300

    feat: added remove function by position in array

--- 04-08-2023/exercise-one/main.cpp
+++ 04-08-2023/exercise-one/main.cpp
@@ -9,12 +9,12 @@ struct List {
     int lastIndex;
 };
 
-void initialize(List &list){
+void initialize(List &list) {
     list.lastIndex = -1;
 }
 
 // sort default: '' - (no sort) | 'C' - crescent | 'D' - decrescent
-bool insert(List &list, char value, char sort = ' '){
+bool insert(List &list, char value, char sort = ' ') {
     int position;
     
     if (list.lastIndex == (MAXIMUM - 1)) return false;
@@ -53,43 +53,51 @@ bool insert(List &list, char value, char sort = ' '){
 // lista => lista a ser usada na pesquisa
 // valor => valor a ser procurado
 // retorna a posição do valor ou -1 se não achou
-int search(List list, char value){
+int search(List list, char value) {
 }
 
 // lista => lista a ser usada na retirada
 // valor => valor a ser retirado
 // retorna true (retirada ok) ou false (retirada falhou)
-bool remove(List &list, char value){
+bool remove(List &list, char value) {
 }
 
-// lista => lista a ser usada na retirada
-// pos => posicao do valor a ser retirado
-// retorna true (retirada ok) ou false (retirada falhou)
-bool remove(List &list, int position){
+bool remove(List &list, int position) {
+    if (position < 0 || position > list.lastIndex) return false;
+    
+    list.lastIndex--;
+    for(int index = position; index <= list.lastIndex; index++) { 
+        list.array[index] = list.array[index + 1];
+    }
+    
+    return true;
 }
 
-void show(List list){
+void show(List list) {
     for(int index = 0; index <= list.lastIndex; index++) { 
         cout << list.array[index] << " ";
     }
 }
 
-int main(){
+int main() {
     List list;
     
     initialize(list);
     
-    insert(list, 'P', 'D');
-    insert(list, 'E', 'D');
-    insert(list, 'R', 'D');
-    insert(list, 'N', 'D');
-    insert(list, 'A', 'D');
-    insert(list, 'M', 'D');
-    insert(list, 'B', 'D');
-    insert(list, 'U', 'D');
-    insert(list, 'C', 'D');
-    insert(list, 'O', 'D');
+    insert(list, 'P');
+    insert(list, 'E');
+    insert(list, 'R');
+    insert(list, 'N');
+    insert(list, 'A');
+    insert(list, 'M');
+    insert(list, 'B');
+    insert(list, 'U');
+    insert(list, 'C');
+    insert(list, 'O');
 
-    cout << endl << "List: ";
+    const bool wasRemoved = remove(list, 10);
+    cout << (wasRemoved ? "true" : "false") << endl;
+    
+    cout << "List: ";
     show(list);
 }
