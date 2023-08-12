93a4662eca1542b9fd54c180e3897904c607468f
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-08-11 21:54:38 -0300

    feat: starting node with connect's next and previous

---
+++ 08-08-2023/exercise-two/main.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    char data;
+    Node *next, *previous;
+};
+
+struct Lue {
+    Node *start, *end;
+};
+
+void initialize(Lue &lue) {
+    lue.start = NULL;
+    lue.end = NULL;
+}
+
+void reset(Lue &list) {
+    Node *auxiliar = list.start;
+    Node *otherAuxiliar;
+
+    while (auxiliar != NULL) {
+        otherAuxiliar = auxiliar->next;
+        delete auxiliar;
+        auxiliar = otherAuxiliar;
+    }
+}
+
+void show(Lue &list, char sort = 'C') {
+    Node *auxiliar = sort == 'C' ? list.start : list.end;
+    
+    while (auxiliar != NULL) {
+        cout << auxiliar->data << " ";
+        auxiliar = sort == 'C' ? auxiliar->next : auxiliar->previous;
+    }
+}
+
+// TODO: Adapter to next and previous
+bool insert(Lue &list, char value) {
+    Node *newNode = new Node;
+    if (newNode == NULL) return false;
+    
+    newNode->data = value;
+    newNode->connect = NULL;
+
+    const bool isEmptyList = list.start == NULL || list.end == NULL;
+    if (isEmptyList) {
+        list.start = newNode;
+        list.end = newNode;
+        return true;
+    }
+
+    const bool isFirstNode = value < list.start->data;
+    if (isFirstNode) {
+        newNode->connect = list.start;
+        list.start = newNode;
+        return true;
+    }
+
+    const bool isLastNode = value > list.end->data;
+    if (isLastNode){
+        list.end->connect = newNode;
+        list.end = newNode;
+        return true;
+    }
+    
+    Node *auxiliar = list.start;
+
+    while (auxiliar->data < value && auxiliar->connect->data < value) {
+        auxiliar = auxiliar->connect;
+    }
+    
+    newNode->connect = auxiliar->connect;
+    auxiliar->connect = newNode;
+    
+    return true;
+}
+
+Node* search(Lue list, char value, char sort = 'C') {
+    Node *auxiliar = sort == 'C' ? list.start : list.end;
+
+    while (auxiliar->data != value) {
+        auxiliar = sort == 'C' ? auxiliar->next : auxiliar->previous;
+        if (auxiliar == NULL) return NULL;
+    }
+    
+    return auxiliar;
+}
+
+// TODO: Adapter to next and previous
+bool remove(Lue &list, char value) {
+    Node *auxiliar = list.start, *previous = NULL;
+    
+    while (auxiliar != NULL && auxiliar->data != value) {
+        previous = auxiliar;
+        auxiliar = auxiliar->connect;
+    }
+    
+    if (auxiliar == NULL) return false;
+    
+    if (auxiliar == list.start) {
+        list.start = list.start->connect;
+        if (auxiliar == list.end) list.end = NULL;
+    } else {
+        previous->connect = auxiliar->connect;
+        if (auxiliar == list.end) list.end = previous;
+    }
+    
+    delete auxiliar;
+    return true;
+}
+
+int main() {
+    Lue lue;
+    
+    initialize(lue);
+    
+    // insert(lue, 'P');
+    // insert(lue, 'E');
+    // insert(lue, 'R');
+    // insert(lue, 'N');
+    // insert(lue, 'A');
+    // insert(lue, 'M');
+    // insert(lue, 'B');
+    // insert(lue, 'U');
+    // insert(lue, 'C');
+    // insert(lue, 'O');
+
+    cout << "List: ";
+    show(lue);
+    
+    // cout<< endl << search(lue, 'P');
+
+    // remove(lue, 'A');
+    // remove(lue, 'E');
+    // remove(lue, 'O');
+    // remove(lue, 'U');
+    // cout << endl << "Removed list: ";
+    // show(lue);
+    
+    reset(lue);
+    
+    return 0;
+}
