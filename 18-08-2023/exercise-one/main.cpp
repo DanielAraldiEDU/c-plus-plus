b06b593a9b4589b4530ad7eb31ce2da9edec3025
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-08-18 20:09:47 -0300

    feat: studying stack list

---
+++ 18-08-2023/exercise-one/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+#define LENGTH 1000 
+
+template<typename T> 
+struct Stack {
+    T array[LENGTH];
+    int topLevel;
+};
+
+template<typename T> 
+void initialize(Stack<T> &stack) {
+    stack.topLevel = -1;
+}
+
+template<typename T>
+T pop(Stack<T> &stack) {
+    T value;
+    if (stack.topLevel < 0) return NULL;
+    
+    value = stack.array[stack.topLevel];
+    stack.topLevel--;
+    return value;
+}
+
+template<typename T>
+void push(Stack<T> &stack, T value) {
+    stack.topLevel++;
+    stack.array[stack.topLevel] = value;
+}
+
+template<typename T>
+void show(Stack<T> &stack) {
+    for (int index = 0; index <= stack.topLevel; index++) {
+        cout << stack.array[index] << " ";
+    }    
+}
+
+int main() {
+    Stack<char> stack;
+    
+    initialize(stack);
+    
+    push<char>(stack, 'P');
+    push<char>(stack, 'U');
+    push<char>(stack, 'S');
+    push<char>(stack, 'H');
+    
+    cout << "Pushed Stack: ";
+    show(stack);
+    
+    cout << endl << "Poped Stack: " << pop<char>(stack);
+    
+    cout << endl << "Stack: ";
+    show(stack);
+    
+    return 0;
+}
