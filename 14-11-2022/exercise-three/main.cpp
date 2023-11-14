4c1fbb8299f7254fc76385daa3455b46f84c66f7
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-11-14 20:23:55 -0300

    temp: removed unnecesseries code blocks

--- 14-11-2022/exercise-three/main.cpp
+++ 14-11-2022/exercise-three/main.cpp
@@ -6,59 +6,48 @@
 using namespace std;
 
 template <typename T>
-T createRandomNumber(T number)
-{
+T createRandomNumber(T number) {
   return fmod(rand(), number);
 }
 
 template <typename T>
-void handleSwapArrayElement(T *elementForExchange, T *element)
-{
+void handleSwapArrayElement(T *elementForExchange, T *element) {
   T value = *elementForExchange;
   *elementForExchange = *element;
   *element = value;
 }
 
 template <typename T>
-T *handleSelectionSort(T *array, int length)
-{
+T *handleSelectionSort(T *array, int length) {
   int minimumIndex, auxiliaryIndex;
 
   for (int index = 0; index < length - 1; index++)
   {
     minimumIndex = index;
-    for (auxiliaryIndex = index + 1; auxiliaryIndex < length; auxiliaryIndex++)
-      if (array[auxiliaryIndex] < array[minimumIndex])
-        minimumIndex = auxiliaryIndex;
-
-    if (minimumIndex != index)
-      handleSwapArrayElement<T>(&array[minimumIndex], &array[index]);
+    for (auxiliaryIndex = index + 1; auxiliaryIndex < length; auxiliaryIndex++) {
+        if (array[auxiliaryIndex] < array[minimumIndex]) minimumIndex = auxiliaryIndex;
+    }
+    if (minimumIndex != index) handleSwapArrayElement<T>(&array[minimumIndex], &array[index]);
   }
 
   return array;
 }
 
 template <typename T>
-T *handlePopulateArray(T *array, int length)
-{
-  for (int i = 0; i < length; i++)
-    array[i] = createRandomNumber<T>(100);
+T *handlePopulateArray(T *array, int length) {
+  for (int i = 0; i < length; i++) array[i] = createRandomNumber<T>(100);
   return array;
 }
 
 template <typename T>
-void printArray(T *array, int length)
-{
-  for (int i = 0; i < length; i++)
-    i + 1 == length ? cout << array[i] : cout << array[i] << " ";
+void printArray(T *array, int length) {
+  for (int i = 0; i < length; i++) i + 1 == length ? cout << array[i] : cout << array[i] << " ";
 }
 
-int main()
-{
+int main() {
   srand(time(NULL));
 
   int *integers = new int[LENGTH];
-  double *doubles = new double[LENGTH];
 
   integers = handlePopulateArray<int>(integers, LENGTH);
   printArray<int>(integers, LENGTH);
@@ -67,17 +56,6 @@ int main()
 
   integers = handleSelectionSort<int>(integers, LENGTH);
   printArray<int>(integers, LENGTH);
-
-  cout << endl;
-  cout << endl;
-
-  doubles = handlePopulateArray<double>(doubles, LENGTH);
-  printArray<double>(doubles, LENGTH);
-
-  cout << endl;
-
-  doubles = handleSelectionSort<double>(doubles, LENGTH);
-  printArray<double>(doubles, LENGTH);
 
   return 0;
 }
