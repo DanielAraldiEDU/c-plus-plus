4312bfd3aefd9f0637a8a9aca761adcdbc0759c8
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-11-14 20:07:42 -0300

    fix: adjusts problem in quick sort function in header

--- 14-11-2022/exercise-four/main.h
+++ 14-11-2022/exercise-four/main.h
@@ -1,15 +1,4 @@
 #define LENGTH 10
 
-template <typename T>
-T createRandomNumber(T number);
-template <typename T>
-T *handleQuickSort(T *array, int start, int end);
-template <typename T>
-T *handlePopulateArray(T *array, int length);
-template <typename T>
-int handleSplitTheArray(T *array, int start, int end);
-template <typename T>
-void handleSwapArrayElement(T *elementForExchange, T *element);
-
-template <typename T>
-void printArray(T *array, int length);
+void handleQuickSort(int array[], int left, int right);
+void printArray(int array[]);
