7cd839cb1037ab7dbae18f1781023ba865abf380
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2023-11-14 20:08:43 -0300

    fix: adjusts problem in quick sort function in `cpp` file

--- 14-11-2022/exercise-four/main.cpp
+++ 14-11-2022/exercise-four/main.cpp
@@ -1,95 +1,46 @@
 #include <iostream>
-#include <time.h>
-#include <math.h>
+#include <string>
 #include "main.h"
 
 using namespace std;
 
-template <typename T>
-T createRandomNumber(T number)
-{
-  return fmod(rand(), number);
+void printArray(int array[]) {
+    for (int index = 0; index < LENGTH; index++) cout << array[index] << " ";
+    cout << endl;
 }
 
-template <typename T>
-void handleSwapArrayElement(T *elementForExchange, T *element)
-{
-  T value = *elementForExchange;
-  *elementForExchange = *element;
-  *element = value;
-}
-
-template <typename T>
-int handleSplitTheArray(T *array, int start, int end)
-{
-  T pivot = array[end];
-  int greaterElement = (start - 1);
-
-  for (int j = start; j < end; j++)
-  {
-    if (array[j] <= pivot)
-    {
-      greaterElement++;
-      handleSwapArrayElement(&array[greaterElement], &array[j]);
-    }
-  }
-
-  handleSwapArrayElement(&array[greaterElement + 1], &array[end]);
-
-  return (greaterElement + 1);
-}
-
-template <typename T>
-T *handleQuickSort(T *array, int start, int end)
-{
-  if (start >= end)
-    return array;
-
-  int index = handleSplitTheArray(array, start, end);
-  handleQuickSort(array, start, index - 1);
-  handleQuickSort(array, index + 1, end);
-}
-
-template <typename T>
-T *handlePopulateArray(T *array, int length)
-{
-  for (int i = 0; i < length; i++)
-    array[i] = createRandomNumber<T>(100);
-  return array;
-}
-
-template <typename T>
-void printArray(T *array, int length)
-{
-  for (int i = 0; i < length; i++)
-    i + 1 == length ? cout << array[i] : cout << array[i] << " ";
+void handleQuickSort(int array[], int left, int right) {
+    int auxiliar, auxiliarLeft, auxiliarRight, pivot;
+    
+    auxiliarLeft = left;
+    auxiliarRight = right;
+    pivot = array[(left + right) / 2];
+    
+    do {
+        while (pivot > array[auxiliarLeft]) auxiliarLeft++;
+        while (pivot < array[auxiliarRight]) auxiliarRight--;
+        
+        if (auxiliarLeft <= auxiliarRight) {
+            auxiliar = array[auxiliarLeft];
+            array[auxiliarLeft] = array[auxiliarRight];
+            array[auxiliarRight] = auxiliar;
+            
+            auxiliarLeft++;
+            auxiliarRight--;
+        }
+    } while (left > right);
+    
+    if (left < auxiliarRight) handleQuickSort(array, left, auxiliarRight);
+    if (right > auxiliarLeft) handleQuickSort(array, auxiliarLeft, right);
 }
 
 int main()
 {
-  srand(time(NULL));
-
-  int *integers = new int[LENGTH];
-  double *doubles = new double[LENGTH];
-
-  integers = handlePopulateArray<int>(integers, LENGTH);
-  printArray<int>(integers, LENGTH);
-
-  cout << endl;
-
-  integers = handleQuickSort<int>(integers, 0, 9);
-  printArray<int>(integers, LENGTH);
-
-  cout << endl;
-  cout << endl;
-
-  doubles = handlePopulateArray<double>(doubles, LENGTH);
-  printArray<double>(doubles, LENGTH);
-
-  cout << endl;
-
-  doubles = handleQuickSort<double>(doubles, 0, 9);
-  printArray<double>(doubles, LENGTH);
-
+  int integers[LENGTH] = {7, 10, 15, -1, 2, 21, 18, 5, 9, -1};
+  
+  printArray(integers);
+  handleQuickSort(integers, 0, LENGTH - 1);
+  printArray(integers);
+  
   return 0;
 }
